057e8bb7a431b5887a2394758755dec6729cd6fd
Phiwa Madinga <phiwa@koedr.org>
2022-04-12 21:26:05 +0200

    mock ui design

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head"
+import Link from "next/link"
+import styles from '../styles/Home.module.css'
 
 const Home = () =>{
   return(
@@ -6,7 +8,16 @@ const Home = () =>{
       <Head>
         <title>Phiwa | Articles</title>
       </Head>
-      <div>it works</div>
+      <div className={`container`}>
+        <div className={styles.blog}>
+          <h2 className={styles.title}>Title</h2>
+          <span className={styles.blog_info}>10 May 2022 - Author</span>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iusto laborum alias quaerat</p>
+          <Link href={"#"}>
+            <a>Read more</a>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
