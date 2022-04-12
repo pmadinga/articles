d1e1f89582e8c4d136ab058519aa84549e73e4f3
Phiwa Madinga <phiwa@koedr.org>
2022-04-12 20:57:53 +0200

    Add create page

--- components/Siteheader.js
+++ components/Siteheader.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import * as styles from "../styles/Site.module.css"
 
 const Siteheader = () =>{
     return(
         <div className={styles.siteheader}>
             <h1>Articles</h1>
+            <Link href={"./create"}>
+                <a>Create a Post</a>
+            </Link>
         </div>
     )
 }
---
+++ pages/create.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Create = () =>{
+    return(    
+        <>
+            <Head>
+                <title>Articles | Create</title>
+            </Head>
+            <div className="container">
+                <h1>Publish an article</h1>
+            </div>
+        </>
+    )
+}
+export default Create;
