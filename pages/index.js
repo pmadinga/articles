49d72897206fbb36c0366d67a24daaaeda7abc65
Phiwa Madinga <phiwa@koedr.org>
2022-04-12 21:57:03 +0200

    Query sql

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,39 @@
+import { PrismaClient } from "@prisma/client"
 import Head from "next/head"
 import Link from "next/link"
 import styles from '../styles/Home.module.css'
 
-const Home = () =>{
+const prisma = new PrismaClient()
+
+const Home = ({ data }) =>{
+  console.log(data);
   return(
     <>
       <Head>
         <title>Phiwa | Articles</title>
       </Head>
       <div className={`container`}>
-        <div className={styles.blog}>
-          <h2 className={styles.title}>Title</h2>
-          <span className={styles.blog_info}>10 May 2022 - Author</span>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iusto laborum alias quaerat</p>
-          <Link href={"#"}>
-            <a>Read more</a>
-          </Link>
-        </div>
+        
+        {data.map(item => (
+            <div className={styles.blog} key={item.id}>
+              <h2 className={styles.title}>{item.title}</h2>
+              <span className={styles.blog_info}>{item.author}</span>
+              <p className={styles.desc}>{item.body}</p>
+              <Link href={`/blog/`}>
+                <a>Read more</a>
+              </Link>
+            </div>
+        ))}
       </div>
     </>
   )
 }
 export default Home
+
+export async function getServerSideProps(){
+  const blogs = await prisma.blog.findMany();
+
+  return {
+    props: { data: blogs}
+  }
+}
